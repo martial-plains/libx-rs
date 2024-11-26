7ad3f581aeef52691cbdc1382cab73024ad19f8d
martial-plains <41553768+martial-plains@users.noreply.github.com>
2024-11-26 18:05:44 -0400

    implement `Numeric` and `SignedNumeric`

--- src/num/traits/basic.rs
+++ src/num/traits/basic.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, AddAssign, Sub, SubAssign};
+use std::ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign};
 
 pub trait AdditiveArithmetic:
     Sized + Add<Output = Self> + Add + AddAssign + Sub<Output = Self> + Sub + SubAssign
@@ -37,6 +37,7 @@ impl AdditiveArithmetic for u16 {
 impl AdditiveArithmetic for u32 {
     const ZERO: Self = 0;
 }
+
 impl AdditiveArithmetic for u64 {
     const ZERO: Self = 0;
 }
@@ -51,4 +52,76 @@ impl AdditiveArithmetic for f32 {
 
 impl AdditiveArithmetic for f64 {
     const ZERO: Self = 0.0;
+}
+
+pub trait Numeric: AdditiveArithmetic + Mul + MulAssign {}
+
+impl Numeric for i8 {}
+
+impl Numeric for i16 {}
+
+impl Numeric for i32 {}
+
+impl Numeric for i64 {}
+
+impl Numeric for i128 {}
+
+impl Numeric for u8 {}
+
+impl Numeric for u16 {}
+
+impl Numeric for u32 {}
+
+impl Numeric for u64 {}
+
+impl Numeric for u128 {}
+
+impl Numeric for f32 {}
+
+impl Numeric for f64 {}
+
+pub trait SignedNumeric: Numeric {
+    fn negate(&mut self);
+}
+
+impl SignedNumeric for i8 {
+    fn negate(&mut self) {
+        *self = self.neg();
+    }
+}
+
+impl SignedNumeric for i16 {
+    fn negate(&mut self) {
+        *self = self.neg();
+    }
+}
+
+impl SignedNumeric for i32 {
+    fn negate(&mut self) {
+        *self = self.neg();
+    }
+}
+
+impl SignedNumeric for i64 {
+    fn negate(&mut self) {
+        *self = self.neg();
+    }
+}
+
+impl SignedNumeric for i128 {
+    fn negate(&mut self) {
+        *self = self.neg();
+    }
+}
+
+impl SignedNumeric for f32 {
+    fn negate(&mut self) {
+        *self = self.neg();
+    }
+}
+
+impl SignedNumeric for f64 {
+    fn negate(&mut self) {
+        *self = self.neg();
+    }
 }
